91bceb8e4b79693954c5080c03a1810a9b0c98c0
manel <manel@nocompila.com>
2016-11-26 14:48:25 +0100

    Added SpringBoot Application

---
+++ SpringBootRules/src/main/java/rules/SpringBootRules.java
@@ -0,0 +1,13 @@
+package rules;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class SpringBootRules{
+    public static void main (String [] args) throws Exception{
+        SpringApplication.run(SpringBootRules.class, args);
+    }
+}
