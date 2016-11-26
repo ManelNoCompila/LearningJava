69817ab72e78dc2655353329bdb8c74aab60e226
manel <manel@nocompila.com>
2016-11-26 17:32:18 +0100

    Added RequestMapping for all site

--- SpringBootRules/src/main/java/rules/SpringBootRules.java
+++ SpringBootRules/src/main/java/rules/SpringBootRules.java
@@ -2,12 +2,16 @@ package rules;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-//import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
 
-//@RestController
+@RestController
 @EnableAutoConfiguration
 public class SpringBootRules{
     public static void main (String [] args) throws Exception{
         SpringApplication.run(SpringBootRules.class, args);
+    }
+    @RequestMapping("/**")
+    String allSite (){
+        return "Spring Boot Rules!!!";
     }
 }
